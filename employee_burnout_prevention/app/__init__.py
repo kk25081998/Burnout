d419c1573d98dcafb9ff471d4fb63cdc3cc1c9c8
kartikkhosa25 <82739151+kartikkhosa25@users.noreply.github.com>
2023-06-21 23:06:44 -0400

    Login stuff

--- employee_burnout_prevention/app/__init__.py
+++ employee_burnout_prevention/app/__init__.py
@@ -1,1 +1,14 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'  # Use sqlite for simplicity
+db = SQLAlchemy(app)
+login = LoginManager(app)
+
+from app.models import User
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
