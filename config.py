287a555b5183ee68b5389a4d23ec277173fcf6e3
kartikkhosa25 <82739151+kartikkhosa25@users.noreply.github.com>
2023-08-18 22:22:12 -0400

    Migration

--- config.py
+++ config.py
@@ -4,8 +4,19 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key_here'
+    SECRET_KEY = os.environ.get('9f87d3b7f4d6ad2d8e8f76f70c9a1b65a1db7f4cb8e9f3e7a9d8f7b6c4a3b291') or 'your_secret_key_here'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
                               'sqlite:///' + os.path.join(basedir, 'app.db')
+    # SQLALCHEMY_DATABASE_URI = 'postgresql://myuser:mypassword@localhost:5432/test'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = True
+
+'''
+postgres=# CREATE DATABASE test;
+CREATE DATABASE
+postgres=# CREATE USER myuser WITH PASSWORD 'mypassword';
+CREATE ROLE
+postgres=# GRANT ALL PRIVILEGES ON DATABASE test TO myuser;
+GRANT
+postgres=# \q
+'''
